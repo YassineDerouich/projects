4df7ee90874236eb2cbdfbd7d07519f719cb5daa
Yassine DEROUICH <97849927+YassineDerouich@users.noreply.github.com>
2022-11-20 13:55:50 +0100

    Update ec2.tf

--- Cloud_DevOps/Terraform/projet_1/ec2.tf
+++ Cloud_DevOps/Terraform/projet_1/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "linux" {
 
     ami = "ami-09d3b3274b6c5d4aa"
-    instance_type = var.instance_type
+    instance_type = t2.micro
     subnet_id = aws_subnet.private.id
 
     iam_instance_profile = aws_iam_instance_profile.ssm-allow.name
